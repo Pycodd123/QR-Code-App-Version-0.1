02d4d1678c046e69caa4ee72f75ddd5f6f1e2985
Pycodd123 <parth.amradkar@gmail.com>
2021-11-23 08:56:34 -0800

    comm

--- CodeDO NOT TOUCH/QRGUI.py
+++ CodeDO NOT TOUCH/QRGUI.py
@@ -1,14 +1,18 @@
+import code
 import tkinter as tk
 import time
 import csv 
-        
-        
+import codetiming
+
+list1 = [1,2,3,4,5,6,7,8,9,10]
+
 import cv2
 
 root = tk.Tk()
 
 root.title("Race Builder version 0.1")
 root.minsize= (800,400)
+
 def startrace():
     
      Window1 = tk.Tk()
@@ -23,6 +27,7 @@ def startrace():
 button1 = tk.Button(root, text="Start Race", width=10, command=startrace).pack()
 
 def add1():
+        
     
 
         
@@ -32,20 +37,29 @@ def add1():
         
 
 
-        
+        codetiming.Timer.start()
         def video_reader():
             cam = cv2.VideoCapture(0)
             detector = cv2.QRCodeDetector()
             while True:
+                
+                
+                
                 _, img = cam.read()
                 data, bbox, _ = detector.detectAndDecode(img)
                 if data:
+                    
                     print("QR Code detected-->", data)
                     with open('Race1.csv', mode='a') as iris:
+                        
+
+                        ttime = codetiming.Timer.last
                         iris_writer = csv.writer(iris, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-                        iris_writer.writerow([data, "place not verified", "time not verified"])
+                        iris_writer.writerow([data, "place not verified" ,ttime])
+
                         time.sleep(1)
+                        
                         
                 
                 cv2.imshow("img", img) 
